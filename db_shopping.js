c1a5eb7aa5b1a7dfba80f8e2f8697b2afde9b14e
Joseph Bryan <josephbryan1987@gmail.com>
2025-07-22 21:29:44 +0800

    Shopping and Booking Update

---
+++ db_shopping.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql');
+const util = require('util');
+
+//local connection
+var con = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'shdb001'
+});
+
+//live connecttion
+// var con = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'pearspor_root',
+//     password : '10061987Yanna$',
+//     database : 'pearspor_db001'
+// });
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+  // Enable promise-based query
+con.query = util.promisify(con.query);
+
+module.exports = con;
